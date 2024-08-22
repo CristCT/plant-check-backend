dc66b510f64d49b139f764bfbf447f37ce583def
ccuevas <ccuevas@previred.com>
2024-08-22 02:55:15 -0400

    add new routes

--- app/monitor_routes.py
+++ app/monitor_routes.py
@@ -1,4 +1,5 @@
-from flask import jsonify
+from flask import request, jsonify
+from datetime import datetime
 from .db import get_db_connection
 
 def fetch_data_from_table(table_name):
@@ -27,3 +28,38 @@ def handle_fetch_invernadero_data():
 
 def handle_fetch_monitoreo_plantas_data():
     return fetch_data_from_table('monitoreo_plantas')
+
+def insert_monitoreo_plantas_data():
+    connection = get_db_connection()
+    try:
+        data = request.get_json()
+
+        if not isinstance(data, list):
+            return jsonify({'error': 'Los datos enviados no son una lista'}), 400
+
+        with connection.cursor() as cursor:
+            for result in data:
+                idcamara = result.get('idcamara')
+                resultado_modelo = result.get('resultado_modelo')
+                planta = result.get('planta')
+                healthy = result.get('healthy')
+                time_str = result.get('time')
+                fecha_registro_str = result.get('fecha_registro')
+                valido = result.get('valido', True)
+
+                time = datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%S.%fZ").strftime('%Y-%m-%d %H:%M:%S')
+                fecha_registro = datetime.strptime(fecha_registro_str, "%Y-%m-%dT%H:%M:%S.%fZ").strftime('%Y-%m-%d %H:%M:%S')
+
+                cursor.execute("""
+                    INSERT INTO monitoreo_plantas (idcamara, resultado_modelo, planta, healthy, time, fecha_registro, valido)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """, (idcamara, resultado_modelo, planta, healthy, time, fecha_registro, valido))
+
+            connection.commit()
+
+        return jsonify({"message": "Resultados guardados exitosamente"}), 201
+    except Exception as e:
+        connection.rollback()
+        return jsonify({"error": str(e)}), 500
+    finally:
+        connection.close()
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import current_app as app
 from .predict_routes import handle_predict
-from .monitor_routes import handle_fetch_camara_data, handle_fetch_invernadero_data, handle_fetch_monitoreo_plantas_data
+from .monitor_routes import handle_fetch_camara_data, handle_fetch_invernadero_data, handle_fetch_monitoreo_plantas_data, insert_monitoreo_plantas_data
 
 # Ruta para predicciones
 @app.route('/predict', methods=['POST'])
@@ -19,4 +19,9 @@ def fetch_invernadero_data():
 @app.route('/monitoreo_plantas/monitoreo', methods=['GET'])
 def fetch_monitoreo_plantas_data():
     return handle_fetch_monitoreo_plantas_data()
+
+# Ruta para guardar resultados de monitoreo de plantas
+@app.route('/monitoreo_plantas/guardarResultados', methods=['POST'])
+def save_results():
+    return insert_monitoreo_plantas_data()
 
